1a4b631dd8ae7da016470be76221f0526b69e27a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-08-06 10:29:39 -0300

    Create URI_2344.cpp

---
+++ URI_2344.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n==0) cout<<"E"<<endl;
+    else if(n>=1 && n<=35) cout<<"D"<<endl;
+    else if(n>=36 && n<=60) cout<<"C"<<endl;
+    else if(n>=61 && n<=85) cout<<"B"<<endl;
+    else if(n>=86 && n<=100) cout<<"A"<<endl;
+}
