984704b60609ff7f6468e204fbf9df228b59cd1f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-08-10 09:20:51 -0300

    Create URI_2510.cpp

---
+++ URI_2510.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+main() {
+  int n;
+  string s;
+  cin >> n;
+  while(n--){
+      cin >> s;
+      cout << ((s != "Batman")? "Y" : "N") << endl;
+  }
+}
