bba51284c229caeac7d61797bea5a80efa65d42d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-08-13 12:21:56 -0300

    Create URI_2533.cpp

---
+++ URI_2533.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main(){
+	int M, N, C, i, x, y;
+	while(scanf("%d", &M) != EOF){
+		x = y = 0;
+		for(i = 0; i < M; i++){
+			scanf("%d %d", &N, &C);
+			x += N * C;
+			y += C;
+		}
+		y *= 100;
+		printf("%.4lf\n", (double)x / (double)y);
+	}
+}
