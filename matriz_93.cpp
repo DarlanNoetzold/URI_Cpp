cdec53d883c7aaf2176a0e7d68d8fbe90d5212b3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-11-13 10:32:50 -0300

    .

--- matriz_93.cpp
+++ matriz_93.cpp
@@ -6,7 +6,7 @@ using namespace std;
 main(){
     srand(time(NULL));
     setlocale(LC_ALL, "portuguese-brazilian");
-    int m[5][5], somaCol3=0, somaLin4=0,somaDiagP=0;
+    int m[5][5], somaCol3=0, somaLin4=0,somaDiagP=0,somaDiagS=0;
 
     for(int i =0; i < 5;i++){
         for(int l = 0; l < 5;l++){
@@ -20,6 +20,9 @@ main(){
             if(i == l){
                 somaDiagP = somaDiagP + m[i][l];
             }
+            if((l + i) == 4){
+                somaDiagS = somaDiagS + m[i][l];
+            }
         }
     }
     for(int i =0; i < 5;i++){
@@ -32,6 +35,7 @@ main(){
     cout<<"\nSoma da linha 4: "<< somaLin4;
     cout<<"\nSoma da coluna 3: "<< somaCol3;
     cout<<"\nSoma da Diagonal principal: "<< somaDiagP;
+    cout<<"\nSoma da Diagonal secundária: "<< somaDiagS;
 
 }
 
--- matriz_94.cpp
+++ matriz_94.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 
 main(){
-    srand(time(NULL));
     setlocale(LC_ALL, "portuguese-brazilian");
-
+    char m[5][5];
+    for(int i =0; i < 5;i++){
+        for(int l = 0; l < 5;l++){
+            if((i + l) < 4){
+                m[i][l] = 'A';
+            }
+            if((i + l) > 4){
+                m[i][l] = 'B';
+            }
+            if((l + i) == 4){
+                m[i][l] = '0';
+            }
+        }
+    }
+    for(int i =0; i < 5;i++){
+        for(int l = 0; l < 5;l++){
+            cout<<"|"<<m[i][l];
+        }
+        cout<<"|\n";
+    }
 
 }
 
