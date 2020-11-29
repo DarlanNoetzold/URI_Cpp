d10f4b3b30df37b2f76b8bfbfa81804e5b8a60e4
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-11-29 10:09:51 -0300

    .

--- Trabalho2_5.cpp
+++ Trabalho2_5.cpp
@@ -3,38 +3,51 @@
 using namespace std;
 
 
-int LugaresD[12][2];
-int LugaresE[12][2];
+int Lugares[48];
+
 
 void preencherMat(){
-    for(int l = 1; l <= 12; l++){
-        for(int c = 1; c <= 2; c++){
-            LugaresD[l][c] = 0;
-        }
-    }
-    for(int l = 1; l <= 12; l++){
-        for(int c = 1; c <= 2; c++){
-            LugaresE[l][c] = 0;
-        }
+    for(int l = 1; l <= 48; l++){
+        Lugares[l] = 0;
     }
 }
 
 void mostrar(){
-    for(int l = 1; l <= 12; l++){
-        for(int c = 1; c <= 2; c++){
-            cout<<"|"<<LugaresD[l][c]<<"--"<<LugaresE[l][c];
+    int cont = 1;
+    for(int l = 1; l <= 48; l++){
+        if(cont == 2){
+            cout<<Lugares[l]<<"|--|";
+        }else if(cont==3){
+            cout<<Lugares[l+1]<<"|"<<Lugares[l]<<"|\n";
+            cont=0;
+            l++;
+        }else{
+            cout<<"|"<<Lugares[l]<<"|";
         }
-        cout<<"|\n";
+        cont++;
+    }
+}
+
+void mostraPos(){
+    int cont = 1;
+    for(int l = 1; l <= 48; l++){
+        if(cont == 2){
+            cout<<l<<"|--|";
+        }else if(cont==3){
+            cout<<l+1<<"|"<<l<<"|\n";
+            cont=0;
+            l++;
+        }else{
+            cout<<"|"<<l<<"|";
+        }
+        cont++;
     }
 }
 
 bool testaOni(){
     int cont = 0;
-    for(int l = 1; l <= 12; l++){
-        for(int c = 1; c <= 2; c++){
-            cont += LugaresD[l][c];
-            cont += LugaresE[l][c];
-        }
+    for(int l = 1; l <= 48; l++){
+        cont += Lugares[l];
     }
     if(cont==48){
         cout<<"Onibus Lotado\n";
@@ -44,18 +57,19 @@ bool testaOni(){
 }
 
 
-void vendPas(int lug){
+void vendPas(int lug, int lado){
     if(testaOni()){
-        for(int l = 1; l <= 12; l++){
-            for(int c = 1; c <= 2; c++){
-                if((l*c) == lug){
-                    if(Lugares[l][c]==0){
-                        Lugares[l][c] = 1;
-                        mostrar();
-                    }else{
-                        cout<<"Poltrona Ocupada!";
-                    }
-                }
+        if(Lugares[lug] == 1){
+            cout<<"Poltrona Ocupada!\n";
+        }else{
+            if(lado == 1 && lug%2==1){
+                Lugares[lug] = 1;
+                cout<<"Venda efetivada\n";
+            }else if(lado == 2 && lug%2==0){
+                Lugares[lug] = 1;
+                cout<<"Venda efetivada\n";
+            }else{
+                cout<<"Opção de lado inválida!\n";
             }
         }
     }
@@ -70,19 +84,26 @@ main(){
         cout<<"Menu: \n";
         cout<<"1 - Vender Passagem\n";
         cout<<"2 - Mostrar mapa de ocupação do ônibus\n";
-        cout<<"3 - Encerrar\n";
+        cout<<"3 - Mostrar mapa de posições\n";
+        cout<<"4 - Encerrar\n";
         cout<<"Digite o número: ";
         cin>>op;
         if(op == 1){
             int lug, lado;
             cout<<"Digite o lugar de sua escolha (0 - 48): ";
             cin>>lug;
-            vendPas(lug);
+            cout<<"Escolha 1 para o lado da janela ou 2 para o lado do corredor: ";
+            cin>>lado;
+            vendPas(lug, lado);
         }else if(op == 2){
             mostrar();
         }else if(op == 3){
+            mostraPos();
+        }else if(op == 4){
             cout<<"\n\nEncerrando...\n\n\n";
             x = 0;
+        }else{
+            cout<<"Opção inválida!\n";
         }
     }
 }
