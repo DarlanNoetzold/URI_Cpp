e4ada5331a97e0d5a6f877a958fbcc7b42b838d4
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-02-13 12:51:37 -0300

    Create ALG2_list2-05.cpp

---
+++ ALG2_list2-05.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main(){
+    string texto1, texto2, texto3;
+
+    cout<<"Digite o primeiro texto: ";
+    getline(cin, texto1);
+    cout<<"Digite o segundo texto: ";
+    getline(cin, texto2);
+    cout<<"Digite o terceiro texto: ";
+    getline(cin, texto3);
+}
