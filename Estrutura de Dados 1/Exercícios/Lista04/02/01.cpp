2c965c601b4d355b6635683089418790e612fb1b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-13 10:20:33 -0300

    Fila Circular

--- Estrutura de Dados 1/Exercícios/Lista04/02/01.cpp
+++ Estrutura de Dados 1/Exercícios/Lista04/02/01.cpp
@@ -7,8 +7,10 @@ using namespace std;
 main(){
     setlocale(LC_ALL, "Portuguese");
 
-    Fila p1;
-    int tamPilha;
+    Fila f;
+    int tamFila;
+    inicializaF(&f, tamFila);
+    Aviao v;
 
 
     int menu = -1, indice = 0;
@@ -35,25 +37,47 @@ main(){
                 break;
             case 1 :
                 system("cls");
-
+                cout<<"Digite o codigo do aviao: ";
+                cin>>v.codigo;
+                cout<<"Digite o numero de passageiros do aviao: ";
+                cin>>v.numPas;
+                cout<<"Digite a empresa do aviao: ";
+                cin>>v.empresa;
+                
+                if(enfileiraF(&f, v)){
+                    cout<<"Aviao inserido!"<<endl;
+                }else{
+                    cout<<"Nao foi possivel enfileirar o aviao, ou a fila esta cheia ou nao foi inicializada!"<<endl;
+                }
 
                 indice++;
                 getchar();
                 break;
             case 2 :
                 system("cls");
-
+                if(desenfileiraF(&f, &v)){
+                    cout<<"O aviao "<<v.codigo<<" decolou!"<<endl;
+                }else{
+                    cout<<"Nao foi possivel desenfileirar o aviao, ou a fila esta vazia ou nao foi inicializada!"<<endl;
+                }
                 getchar();
                 break;
             case 3 :
                 system("cls");
-                
+                cout<<"Codigo dos avioes a espera para decolagem: "<<endl;
+                mostraF(&f);
                 getchar();
                 break;
 
             case 4 :
                 system("cls");
-                
+                if(espiarF(&f, &v)){
+                    cout<<"Seguem as informacoes do proximo avia a decolar: "<<endl;
+                    cout<<"Codigo: \t"<<v.codigo<<endl;
+                    cout<<"Numero de passageiros: \t"<<v.numPas<<endl;
+                    cout<<"Empresa: \t"<<v.empresa<<endl;
+                }
+
                 getchar();
                 break;
             case 5 :
