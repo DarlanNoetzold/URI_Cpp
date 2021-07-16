a0dbb16a36e64c3e194d59e054084712e9f2166a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-16 10:03:26 -0300

    Fila com pilha

---
+++ Estrutura de Dados 1/Exercícios/Lista04/03/02.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "pilha.hpp"
+#include "filacircular.hpp"
+
+main(){
+    setlocale(LC_ALL, "Portuguese");
+    
+    Pilha p1;
+    Fila f1;
+    string str, str2;
+    char valor;
+    cout << "Digite uma string: ";
+    cin >> str;
+
+    inicializa(&p1, str.size());
+
+    for(int i=0; i<str.size(); i++){
+        empilhar(&p1, str.at(i) );
+    }
+
+
+    for(int i=0; i<str.size(); i++){
+
+        if(desempilhar(&p1, &valor ) )
+            str2 += valor;
+    }
+
+
+    cout<<"Inverso da palavra: "<<str2<<endl;
+
+}
+
+
