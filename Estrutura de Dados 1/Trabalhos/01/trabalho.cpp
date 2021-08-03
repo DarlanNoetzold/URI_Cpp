63c1fd0cb4564408c7efd4795d6eb7e3006a065f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-03 11:29:43 -0300

    D pronto

--- Estrutura de Dados 1/Trabalhos/01/trabalho.cpp
+++ Estrutura de Dados 1/Trabalhos/01/trabalho.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #include "Estruturas.hpp"
 
 
-void incluirPessoa(FilaPessoa *filaPessoa){
+void incluirPessoa(FilaPessoa *filaPessoa, FilaPessoa *filaPessoaPrioritarias){
     char auxB;
     string auxS;
     Pessoa p1;
@@ -72,10 +72,15 @@ void incluirPessoa(FilaPessoa *filaPessoa){
             valido = false;
         }
     }while(!valido);
-    enfileiraFilaPessoa(*(&filaPessoa), p1);
+
+    if(p1.gestante || p1.defFisico || p1.idade > 65){
+        enfileiraFilaPessoa(*(&filaPessoaPrioritarias), p1);
+    }else{
+        enfileiraFilaPessoa(*(&filaPessoa), p1);
+    }
 }
 
-void incluirEmpresa(FilaEmpresa *filaEmpresa){
+void incluirEmpresa(FilaEmpresa *filaEmpresa, FilaEmpresa *filaEmpresaPrioritarias){
     char auxB;
     string auxS;
     Empresa p1;
@@ -106,38 +111,70 @@ void incluirEmpresa(FilaEmpresa *filaEmpresa){
             valido = false;
         }
     }while(!valido);
-    enfileiraFilaEmpresa(*(&filaEmpresa), p1);
+
+    if(p1.tipo == "livros"){
+        enfileiraFilaEmpresa(*(&filaEmpresaPrioritarias), p1);
+    }else{
+        enfileiraFilaEmpresa(*(&filaEmpresa), p1);
+    }
 }
 
-void atendePessoa(FilaPessoa *filaPessoa, PilhaFichaDoacao *pilhaFichaDoacao, PilhaObjeto *pilhaLivros, PilhaObjeto *pilhaEquipamentos){
+void atendePessoa(FilaPessoa *filaPessoa, PilhaFichaDoacao *pilhaFichaDoacao, PilhaObjeto *pilhaLivros, PilhaObjeto *pilhaEquipamentos, FilaPessoa *filaPessoaPrioritarias){
     bool valido = false;
     Pessoa valor;
-    desenfileiraFilaPessoa(*(&filaPessoa), &valor);
     FichaDoacao fichaDoacao;
-    fichaDoacao.pessoa = valor;
     Objeto objeto;
     string auxS;
-    do{
-        cout<<"Digite o tipo (livro, equipamento)"<<endl;
-        cin>>auxS;
-        if(auxS == "livro" || auxS == "equipamento"){
-            objeto.tipo = auxS;
-            valido = true;
-        }else{
-            cout<<"Valor invalido!"<<endl;
-            valido = false;
+    if(!vaziaFilaPessoa(filaPessoaPrioritarias)){
+        desenfileiraFilaPessoa(*(&filaPessoaPrioritarias), &valor);
+        fichaDoacao.pessoa = valor;
+        do{
+            cout<<"Digite o tipo (livro, equipamento)"<<endl;
+            cin>>auxS;
+            if(auxS == "livro" || auxS == "equipamento"){
+                objeto.tipo = auxS;
+                valido = true;
+            }else{
+                cout<<"Valor invalido!"<<endl;
+                valido = false;
+            }
+        }while(!valido);
+
+        cout<<"Digite a descricao: "<<endl;
+        cin>>objeto.descricao;
+
+        fichaDoacao.objeto = objeto;
+        empilhaFichaDoacao(*(&pilhaFichaDoacao), fichaDoacao);
+        if(objeto.tipo == "livro"){
+            empilhaObjeto(*(&pilhaLivros), objeto);
+        }else if(objeto.tipo == "equipamento"){
+            empilhaObjeto(*(&pilhaEquipamentos), objeto);
         }
-    }while(!valido);
+    }else{
+        desenfileiraFilaPessoa(*(&filaPessoa), &valor);
+        fichaDoacao.pessoa = valor;
+        do{
+            cout<<"Digite o tipo (livro, equipamento)"<<endl;
+            cin>>auxS;
+            if(auxS == "livro" || auxS == "equipamento"){
+                objeto.tipo = auxS;
+                valido = true;
+            }else{
+                cout<<"Valor invalido!"<<endl;
+                valido = false;
+            }
+        }while(!valido);
 
-    cout<<"Digite a descricao: "<<endl;
-    cin>>objeto.descricao;
+        cout<<"Digite a descricao: "<<endl;
+        cin>>objeto.descricao;
 
-    fichaDoacao.objeto = objeto;
-    empilhaFichaDoacao(*(&pilhaFichaDoacao), fichaDoacao);
-    if(objeto.tipo == "livro"){
-        empilhaObjeto(*(&pilhaLivros), objeto);
-    }else if(objeto.tipo == "equipamento"){
-        empilhaObjeto(*(&pilhaEquipamentos), objeto);
+        fichaDoacao.objeto = objeto;
+        empilhaFichaDoacao(*(&pilhaFichaDoacao), fichaDoacao);
+        if(objeto.tipo == "livro"){
+            empilhaObjeto(*(&pilhaLivros), objeto);
+        }else if(objeto.tipo == "equipamento"){
+            empilhaObjeto(*(&pilhaEquipamentos), objeto);
+        }
     }
     
     ofstream escreve("doacao.txt",ios::app);
@@ -146,13 +183,65 @@ void atendePessoa(FilaPessoa *filaPessoa, PilhaFichaDoacao *pilhaFichaDoacao, Pi
 }
 
 
+void atendeEmpresa(FilaEmpresa *filaEmpresa, FilaEmpresa *filaEmpresaPrioritarias, PilhaObjeto *pilhaLivros, PilhaObjeto *pilhaEquipamentos){
+    Objeto objeto;
+    int quant=0;
+    string verifica;
+    Empresa empresa;
+
+    if(!vaziaFilaEmpresa(filaEmpresaPrioritarias)){
+        desenfileiraFilaEmpresa(*(&filaEmpresaPrioritarias), &empresa);
+        cout<<"Quantos objeto serão retirado: "<<endl;
+        cin>>quant;
+
+        cout<<"Qual o tipo de retirada: (você está em priridade então deve ser livro)"<<endl;
+        cin>>verifica;
+
+        if(verifica == empresa.tipo){
+            for(int i = 0; i < quant; i++){
+                desempilhaObjeto(*(&pilhaLivros), &objeto);
+                ofstream escreve("retirada.txt",ios::app);
+                escreve << empresa.nome <<" # "<< empresa.CNPJ <<" # TRANS_"<< empresa.tipo <<" # OBJ_"<< objeto.tipo <<" # "<<objeto.descricao<<"\n";
+                escreve.close();
+            }
+        }else{
+            cout<<"Tipo errado!"<<endl;
+            getchar();
+        }
+    }else{
+        desenfileiraFilaEmpresa(*(&filaEmpresa), &empresa);
+        cout<<"Quantos objeto serão retirado: "<<endl;
+        cin>>quant;
+
+        cout<<"Qual o tipo de retirada: (você  nao esta em prioridade então deve ser equipamento)"<<endl;
+        cin>>verifica;
+
+        if(verifica == empresa.tipo){
+            for(int i = 0; i < quant; i++){
+                desempilhaObjeto(*(&pilhaLivros), &objeto);
+                ofstream escreve("retirada.txt",ios::app);
+                escreve << empresa.nome <<" # "<< empresa.CNPJ <<" # TRANS_"<< empresa.tipo <<" # OBJ_"<< objeto.tipo <<" # "<<objeto.descricao<<"\n";
+                escreve.close();
+            }
+        }else{
+            cout<<"Tipo errado!"<<endl;
+            getchar();
+        }
+    }
+}
+
 main(){
 
     cout<<"Inicializando filas:"<<endl;
     FilaPessoa filaPessoa;
     inicializaFilaPessoa(&filaPessoa);
+    FilaPessoa filaPessoaPrioritarias;
+    inicializaFilaPessoa(&filaPessoaPrioritarias);
     FilaEmpresa filaEmpresa;
     inicializaFilaEmpresa(&filaEmpresa);
+    FilaEmpresa filaEmpresaPrioritarias;
+    inicializaFilaEmpresa(&filaEmpresaPrioritarias);
+
 
     cout<<"Inicializando pilhas:"<<endl;
     PilhaFichaDoacao pilhaFichaDoacao;
@@ -171,6 +260,10 @@ main(){
         cout << "# 1 - A              #" << endl;
         cout << "# 2 - B              #" << endl;
         cout << "# 3 - C              #" << endl;
+        cout << "# 4 - D              #" << endl;
+        cout << "# 5 - E              #" << endl;
+        cout << "# 6 - F              #" << endl;
+        cout << "# 7 - G              #" << endl;
         cout << "######################" << endl;
         cout << "Sua escolha: ";
         cin >> menu;
@@ -185,24 +278,25 @@ main(){
             case 1 :
                 system("cls");
                 cout<<"Enfielirando pessoa..."<<endl;
-                incluirPessoa(&filaPessoa);
+                incluirPessoa(&filaPessoa, &filaPessoaPrioritarias);
                 getchar();
                 break;
             case 2 :
                 system("cls");
                 cout<<"Enfielirando empresa..."<<endl;
-                incluirEmpresa(&filaEmpresa);
+                incluirEmpresa(&filaEmpresa, &filaEmpresaPrioritarias);
                 getchar();
                 break;
             case 3 :
                 system("cls");
                 cout<<"Atendendo pessoa..."<<endl;
-                atendePessoa(&filaPessoa, &pilhaFichaDoacao, &pilhaLivros, &pilhaEquipamentos);
+                atendePessoa(&filaPessoa, &pilhaFichaDoacao, &pilhaLivros, &pilhaEquipamentos, &filaPessoaPrioritarias);
                 getchar();
                 break;
             case 4 :
                 system("cls");
-
+                cout<<"Atendendo empresa..."<<endl;
+                atendeEmpresa(&filaEmpresa, &filaEmpresaPrioritarias, &pilhaLivros, &pilhaEquipamentos);
                 getchar();
                 break;
             case 5 :
