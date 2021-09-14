f20751537d7435502ebffc4deccb3c3aa2d995d3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-14 08:40:18 -0300

    Lista duplamente encadeada

---
+++ Estrutura de Dados 1/Exercícios/Lista11/01/01.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include ""
+
+main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    Lista *lista = new Lista();
+    int valor, posicao;
+
+    int menu = -1;
+
+    do{
+        system("cls");
+        cout << "###############MENU###############" << endl;
+        cout << "# 0 - Sair                       #" << endl;
+        cout << "# 2 - Inserir                    #" << endl;
+        cout << "# 3 - Remover                    #" << endl;
+        cout << "# 4 - Consultar                  #" << endl;
+        cout << "# 5 - Mostrar                    #" << endl;
+        cout << "# 6 - Insere Pocicao             #" << endl;
+        cout << "##################################" << endl;
+        cout << "Sua escolha: ";
+        cin >> menu;
+        fflush(stdin);//limpa o buffer do teclado
+
+        switch(menu){
+            case 0 :
+                system("cls");
+                cout << "PROGRAMA FINALIZADO";
+                getchar();
+                break;
+            case 2 :
+                system("cls");
+                
+                getchar();
+                break;
+            case 3 :
+                system("cls");
+                
+                getchar();
+                break;
+
+            case 4 :
+                system("cls");
+                
+                getchar();
+                break;
+            case 5 :
+                system("cls");
+                
+                getchar();
+                break;
+            case 6 :
+                system("cls");
+                
+                getchar();
+                break;
+        }
+    }while(menu != 0);
+    
+    delete(lista);
+    
+}
